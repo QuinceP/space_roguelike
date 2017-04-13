43e043c746903b6e74caa2f283c0459a37938d62
quincep <christen@christenward.com>
2017-04-12 22:47:41 -0500

    scan for corridor and add it

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import pygame, sys, math, numpy, random
-from numpy.random import choice
+import math
+import random
+import sys
+from pprint import pprint
+
+import numpy
+import pygame
 from pygame.locals import *
 
 
@@ -62,10 +67,15 @@ class Tile():
         self.z_level = z_level
         self.is_passable = is_passable
 
+    def __repr__(self):
+        return "<Tile name:%s sprite:%s z_level:%s is_passable:%s>" % (
+            self.name, self.sprite, self.z_level, self.is_passable)
+
 
 FLOOR = Tile('floor', 'oryx_16bit_scifi_world_01.png', 1, True)
 GRASS = Tile('grass', 'grass0.png', 1, True)
-WATER = Tile('water', 'water0.png', 1, True)
+WATER = Tile('water', 'water0.png', 1, False)
+DOOR = Tile('door', 'oryx_16bit_scifi_world_481.png', 1, False)
 WALLS = [
     Tile('wall_top_left', 'oryx_16bit_scifi_world_14.png', 1, False),
     Tile('wall_top_right', 'oryx_16bit_scifi_world_15.png', 1, False),
@@ -87,64 +97,178 @@ def create_room(room):
     # go through the tiles in the rectangle and make them passable
     for x in range(room.x1 - 1, room.x2 + 1):
         for y in range(room.y1 - 1, room.y2 + 1):
-            if (x == room.x1 - 1 and y == room.y1 - 1):  # top left corner
-                map[x][y] = WALLS[0]
-            elif (x == room.x1 - 1 and y == room.y2):  # bottom left corner
-                map[x][y] = WALLS[2]
-            elif (x == room.x2 and y == room.y1 - 1):  # top right corner
-                map[x][y] = WALLS[1]
-            elif (x == room.x2 and y == room.y2):  # bottom right corner
-                map[x][y] = WALLS[3]
-            elif (x == room.x1 - 1 or x == room.x2):  # horizontal
-                map[x][y] = WALLS[4]
-            elif (y == room.y1 - 1 or y == room.y2):  # vertical
-                map[x][y] = WALLS[5]
+            if x == room.x1 - 1 and y == room.y1 - 1:  # top left corner
+                tilemap[x][y] = WALLS[0]
+            elif x == room.x1 - 1 and y == room.y2:  # bottom left corner
+                tilemap[x][y] = WALLS[2]
+            elif x == room.x2 and y == room.y1 - 1:  # top right corner
+                tilemap[x][y] = WALLS[1]
+            elif x == room.x2 and y == room.y2:  # bottom right corner
+                tilemap[x][y] = WALLS[3]
+            elif x == room.x1 - 1 or x == room.x2:  # horizontal
+                tilemap[x][y] = WALLS[4]
+            elif y == room.y1 - 1 or y == room.y2:  # vertical
+                tilemap[x][y] = WALLS[5]
             else:
-                map[x][y] = FLOOR
+                tilemap[x][y] = FLOOR
 
 
 def pick_wall(room):
-    # direction = random.choice(['north', 'east', 'south', 'west'])
-    direction = 'east'
+    direction = random.choice(['north', 'east', 'south', 'west'])
+    edge = 0
     x = 0
     y = 0
     if direction == 'north':
         x = random.randint(room.x1, room.x2 - 1)
         y = room.y1 - 1
+
     elif direction == 'east':
         x = room.x2
         y = random.randint(room.y1, room.y2 - 1)
+
     elif direction == 'south':
         x = random.randint(room.x1, room.x2 - 1)
         y = room.y2
+
     elif direction == 'west':
         x = room.x1 - 1
         y = random.randint(room.y1, room.y2 - 1)
 
-    return x, y, direction
+    if x == room.x1 or y == room.y1:
+        edge = 1
+    elif x == room.x2 - 1 or y == room.y2 - 1:
+        edge = 2
+
+    return x, y, direction, edge
+
+
+def create_corridor(wall_x, wall_y, direction, edge, corridor_length):
+    if direction == 'north':
+        if wall_y - corridor_length > 0:
+            for i in range(wall_x - 1, wall_x + 2):
+                for j in range(wall_y - corridor_length, wall_y + 1):
+                    if i == wall_x:
+                        if j == wall_y:
+                            tilemap[i][j] = DOOR
+                        else:
+                            tilemap[i][j] = FLOOR
+                    else:
+                        if j == wall_y:
+                            if i == wall_x + 1:
+                                if edge == 2:
+                                    tilemap[i][j] = WALLS[4]
+                                else:
+                                    tilemap[i][j] = WALLS[2]
+                            elif i == wall_x - 1:
+                                if edge == 1:
+                                    tilemap[i][j] = WALLS[4]
+                                else:
+                                    tilemap[i][j] = WALLS[3]
+                        else:
+                            tilemap[i][j] = WALLS[4]
+    elif direction == 'east':
+        if wall_x + corridor_length < MAPWIDTH:
+            for i in range(wall_x, wall_x + corridor_length):
+                for j in range(wall_y - 1, wall_y + 2):
+                    if j == wall_y:
+                        if i == wall_x:
+                            tilemap[i][j] = DOOR
+                        else:
+                            tilemap[i][j] = FLOOR
+                    else:
+                        if i == wall_x:
+                            if j == wall_y + 1:
+                                if edge == 2:
+                                    tilemap[i][j] = WALLS[5]
+                                else:
+                                    tilemap[i][j] = WALLS[0]
+                            elif j == wall_y - 1:
+                                if edge == 1:
+                                    tilemap[i][j] = WALLS[5]
+                                else:
+                                    tilemap[i][j] = WALLS[2]
+                        else:
+                            tilemap[i][j] = WALLS[5]
+    elif direction == 'south':
+        if wall_y + corridor_length < MAPHEIGHT:
+            for i in range(wall_x - 1, wall_x + 2):
+                for j in range(wall_y, wall_y + corridor_length):
+                    if i == wall_x:
+                        if j == wall_y:
+                            tilemap[i][j] = DOOR
+                        else:
+                            tilemap[i][j] = FLOOR
+                    else:
+                        if j == wall_y:
+                            if i == wall_x + 1:
+                                if edge == 2:
+                                    tilemap[i][j] = WALLS[4]
+                                else:
+                                    tilemap[i][j] = WALLS[0]
+                            elif i == wall_x - 1:
+                                if edge == 1:
+                                    tilemap[i][j] = WALLS[4]
+                                else:
+                                    tilemap[i][j] = WALLS[1]
+                        else:
+                            tilemap[i][j] = WALLS[4]
+    elif direction == 'west':
+        if wall_x - corridor_length > 0:
+            for i in range(wall_x - corridor_length, wall_x + 1):
+                for j in range(wall_y - 1, wall_y + 2):
+                    if j == wall_y:
+                        if i == wall_x:
+                            tilemap[i][j] = DOOR
+                        else:
+                            tilemap[i][j] = FLOOR
+                    else:
+                        if i == wall_x:
+                            if j == wall_y + 1:
+                                if edge == 2:
+                                    tilemap[i][j] = WALLS[5]
+                                else:
+                                    tilemap[i][j] = WALLS[1]
+                            elif j == wall_y - 1:
+                                if edge == 1:
+                                    tilemap[i][j] = WALLS[5]
+                                else:
+                                    tilemap[i][j] = WALLS[3]
+                        else:
+                            tilemap[i][j] = WALLS[5]
 
 
 def scan_for_corridor(wall_x, wall_y, direction):
     corridor_length = random.randint(3, 10)
     if direction == 'north':
-        if (wall_y - corridor_length > 0):
-            for i in range(wall_y - corridor_length, wall_y + 1):
-                map[wall_x][i] = FLOOR
+        if wall_y - corridor_length > 0:
+            for i in range(wall_x - 1, wall_x + 2):
+                for j in range(wall_y - corridor_length, wall_y):
+                    if tilemap[i][j].z_level != 0:
+                        pprint(tilemap[i][j])
+                        return 0
     elif direction == 'east':
-        if (wall_x + corridor_length < MAPWIDTH):
-            for i in range(wall_x, wall_x + corridor_length):
-                map[i][wall_y] = FLOOR
+        if wall_x + corridor_length < MAPWIDTH:
+            for i in range(wall_x + 1, wall_x + corridor_length):
+                for j in range(wall_y - 1, wall_y + 2):
+                    if tilemap[i][j].z_level != 0:
+                        pprint(tilemap[i][j])
+                        return 0
     elif direction == 'south':
-        if (wall_y + corridor_length < MAPHEIGHT):
-            for i in range(wall_y, wall_y + corridor_length):
-                map[wall_x][i] = FLOOR
+        if wall_y + corridor_length < MAPHEIGHT:
+            for i in range(wall_x - 1, wall_x + 2):
+                for j in range(wall_y + 1, wall_y + corridor_length):
+                    if tilemap[i][j].z_level != 0:
+                        pprint(tilemap[i][j].z_level)
+                        return 0
     elif direction == 'west':
-        if (wall_x - corridor_length > 0):
-            for i in range(wall_x - corridor_length, wall_x + 1):
-                map[i][wall_y] = FLOOR
+        if wall_x - corridor_length > 0:
+            for i in range(wall_x - corridor_length, wall_x):
+                for j in range(wall_y - 1, wall_y + 2):
+                    if tilemap[i][j].z_level != 0:
+                        pprint(tilemap[i][j])
+                        return 0
 
-    return True
-
+    return corridor_length
 
 def generate_door(room_x, room_y, room_height, room_width):
     wall_x = random.choice([room_x - 1, room_x + room_width - 1])
@@ -157,8 +281,8 @@ def generate_door(room_x, room_y, room_height, room_width):
 
 
 def generate_tilemap():
-    global map
-    map = [[numpy.random.choice(BACKGROUND, p=BACKGROUND_WEIGHTS) for height in range(MAPHEIGHT)] for width in
+    global tilemap
+    tilemap = [[numpy.random.choice(BACKGROUND, p=BACKGROUND_WEIGHTS) for height in range(MAPHEIGHT)] for width in
            range(MAPWIDTH)]
 
     starting_room_width = 5
@@ -168,10 +292,14 @@ def generate_tilemap():
 
     starting_room = Rect(starting_room_x, starting_room_y, starting_room_width, starting_room_height)
     create_room(starting_room)
-    wall_x, wall_y, direction = pick_wall(starting_room)
-    scan_for_corridor(wall_x, wall_y, direction)
+    wall_x, wall_y, direction, edge = pick_wall(starting_room)
 
-    return map
+    available_cooridor_length = scan_for_corridor(wall_x, wall_y, direction)
+    if available_cooridor_length != 0:
+        create_corridor(wall_x, wall_y, direction, edge, available_cooridor_length)
+    else:
+        print("blocked!")
+    return tilemap
 
 
 # class Player:
@@ -207,7 +335,7 @@ PLAYER = Sprite(['oryx_16bit_scifi_creatures_01.png', 'oryx_16bit_scifi_creature
 
 clock = pygame.time.Clock()
 
-map = generate_tilemap()
+tilemap = generate_tilemap()
 
 while True:
     clock.tick(12)
@@ -216,7 +344,7 @@ while True:
     if (keys[K_RIGHT]) and PLAYER.x < MAPWIDTH - 1:
         PLAYER.facing_right = True
         try:
-            passable = map[PLAYER.x + 1][PLAYER.y].is_passable
+            passable = tilemap[PLAYER.x + 1][PLAYER.y].is_passable
         except IndexError:
             passable = False
 
@@ -225,7 +353,7 @@ while True:
     if (keys[K_LEFT]) and PLAYER.x > 0:
         PLAYER.facing_right = False
         try:
-            passable = map[PLAYER.x - 1][PLAYER.y].is_passable
+            passable = tilemap[PLAYER.x - 1][PLAYER.y].is_passable
         except IndexError:
             passable = False
 
@@ -235,14 +363,14 @@ while True:
     if (keys[K_UP]) and PLAYER.y > 0:
 
         try:
-            passable = map[PLAYER.x][PLAYER.y - 1].is_passable
+            passable = tilemap[PLAYER.x][PLAYER.y - 1].is_passable
         except IndexError:
             passable = False
         if passable:
             PLAYER.move(0, -1)
     if (keys[K_DOWN]) and PLAYER.y < MAPHEIGHT - 1:
         try:
-            passable = map[PLAYER.x][PLAYER.y + 1].is_passable
+            passable = tilemap[PLAYER.x][PLAYER.y + 1].is_passable
         except IndexError:
             passable = False
 
@@ -260,7 +388,7 @@ while True:
 
     for row in range(MAPHEIGHT):
         for column in range(MAPWIDTH):
-            DISPLAYSURF.blit(map[column][row].sprite, (column * TILESIZE, row * TILESIZE))
+            DISPLAYSURF.blit(tilemap[column][row].sprite, (column * TILESIZE, row * TILESIZE))
 
     PLAYER.update()
 
